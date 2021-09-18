a89f507147db08729eb80bde0df65b81e7b460d8
curly08 <mattynoakes08@gmail.com>
2021-09-18 13:56:02 -0400

    Final product!

--- app.rb
+++ app.rb
@@ -1,38 +1,8 @@
-require 'pry-byebug'
-
 def bubble_sort(arr)
-  binding.pry
-  arr.each_with_index.map do | num, index |
-    if arr[index + 1] != nil
-      if num > arr[index + 1]
-        # swap positions
-        arr.insert(index + 2, num)
-        arr.delete_at(index)
-      end
-    end
-  end
-  arr.each_with_index.map do | num, index |
-    if arr[index + 1] != nil
-      if num > arr[index + 1]
-        # swap positions
-        arr.insert(index + 2, num)
-        arr.delete_at(index)
-      end
-    end
-  end
-  arr.each_with_index.map do | num, index |
-    if arr[index + 1] != nil
-      if num > arr[index + 1]
-        # swap positions
-        arr.insert(index + 2, num)
-        arr.delete_at(index)
-      end
-    end
-  end
-  arr.each_with_index.map do | num, index |
-    if arr[index + 1] != nil
-      if num > arr[index + 1]
-        # swap positions
+  for i in 1..(arr.size - 1)
+    arr.each_with_index.map do | num, index |
+      break if (index == arr.size - i)
+      if (arr[index + 1] != nil) && (num > arr[index + 1])
         arr.insert(index + 2, num)
         arr.delete_at(index)
       end
